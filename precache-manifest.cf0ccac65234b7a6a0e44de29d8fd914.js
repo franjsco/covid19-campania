0dacaf998bb774c013a41ac4595a3b95a0f66d95
Francesco Esposito <33671357+franjsco@users.noreply.github.com>
2020-03-14 23:45:30 +0100

    Updates

---
+++ precache-manifest.cf0ccac65234b7a6a0e44de29d8fd914.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "048bae6ec6447d247e91133de9b382a6",
+    "url": "/covid19-campania/index.html"
+  },
+  {
+    "revision": "c1b498bafc5fc155b72f",
+    "url": "/covid19-campania/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "af10d178fc1dd0e47e84",
+    "url": "/covid19-campania/static/css/main.562c9271.chunk.css"
+  },
+  {
+    "revision": "c1b498bafc5fc155b72f",
+    "url": "/covid19-campania/static/js/2.29f21e72.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/covid19-campania/static/js/2.29f21e72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af10d178fc1dd0e47e84",
+    "url": "/covid19-campania/static/js/main.d7ddf31f.chunk.js"
+  },
+  {
+    "revision": "98feadb937f5b153d808",
+    "url": "/covid19-campania/static/js/runtime-main.92308f37.js"
+  }
+]);
