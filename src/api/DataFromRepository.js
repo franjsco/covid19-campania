a9d402ab7e9b9765e3c49899c0067d1955aa9ad3
Francesco Esposito <33671357+franjsco@users.noreply.github.com>
2020-04-07 20:11:00 +0200

    Modificate funzioni per il recupero dei dati

--- src/api/DataFromRepository.js
+++ src/api/DataFromRepository.js
@@ -1,13 +1,8 @@
-// import axios from 'axios';
 import axios from 'axios';
-import csv2json from 'csvjson-csv2json';
 
-import { format, addDays, getHours } from 'date-fns';
-
-
-const getCSVRegionifromRepository = async (date) => {
-  const url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/';
-  const filename = `dpc-covid19-ita-regioni-${format(date, 'yyyyMMdd')}.csv`;
+const getJsonRegioni = async () => {
+  const url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/';
+  const filename = 'dpc-covid19-ita-regioni-latest.json';
   const finalURL = `${url + filename}`;
   let result;
 
@@ -22,35 +17,11 @@ const getCSVRegionifromRepository = async (date) => {
 };
 
 
-const convertCSVtoJSON = (csv) => {
-  if (!csv) {
-    return null;
-  }
-
-  return csv2json(csv, { parseNumbers: true });
-};
-
 const getData = async () => {
-  const actualDate = new Date();
-  const previousDate = addDays(actualDate, -1);
-  const hour = getHours(actualDate);
-  let result;
+  const nomeRegione = 'Campania';
 
-  // preleva il CSV con data attuale solamente se sono passate le 18
-  if (hour >= 18) {
-    result = await getCSVRegionifromRepository(actualDate);
-  }
-
-  // preleva il CSV con data precedente qualora non fosse stato trovato quello attuale
-  if (!result) {
-    result = await getCSVRegionifromRepository(previousDate);
-  }
-
-  // converte il CSV in JSON e filtra i dati per la regione Campania
-  if (result) {
-    const tmpJSON = convertCSVtoJSON(result);
-    result = tmpJSON.find((d) => d.denominazione_regione === 'Campania');
-  }
+  const datiRegione = await getJsonRegioni();
+  const result = datiRegione.find((regione) => regione.denominazione_regione === nomeRegione);
 
   return result;
 };
